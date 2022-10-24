4476b8efa37a9fcdb5f77c646e010aa7504b176a
c3eru <96016081+c3eru@users.noreply.github.com>
2022-10-24 20:56:36 +0700

    Iseng sapa tau kuat

--- build.sh
+++ build.sh
@@ -1,19 +1,17 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/c3eru/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/c3eru/local_manifest --depth 1 -b nad .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/c3eru/local_manifest --depth 1 -b xd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch nad_juice-userdebug
+lunch xdroid_chime-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=mobx
 export KBUILD_BUILD_HOST=builder-ci-task-kang-moment
 export BUILD_USERNAME=mobx
 export BUILD_HOSTNAME=builder-ci-task-kang-moment
-# export USE_MICROG=true
-export USE_GAPPS=true
-mka nad -j8
+make xd -j8
 # end 
 
-# build 11
+# build 1
