3155fa96e5e5108ee86dccafac4eea3eb734ad2e
c3eru <96016081+c3eru@users.noreply.github.com>
2022-10-28 06:51:59 +0700

    Test

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/c3eru.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/c3eru/local_manifest --depth 1 -b c3eru-patch-1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
