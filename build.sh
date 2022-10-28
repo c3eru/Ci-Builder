cd356820173b48e44694481cc5420e5328361a98
c3eru <96016081+c3eru@users.noreply.github.com>
2022-10-28 12:15:26 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b triton -g default,-mips,-darwin,-notdefault
-git clone https://github.com/c3eru/local_manifest --depth 1 -b c3eru-patch-1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/c3eru/local_manifest --depth 1 -b c3eru-patch-1 .repo/local_manifests
 
 # build rom
 source build/envsetup.sh
