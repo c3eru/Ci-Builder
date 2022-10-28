ed675eddad1ec0ee82577a4fdc430c43890e1353
c3eru <96016081+c3eru@users.noreply.github.com>
2022-10-28 11:03:34 +0700

    Awaken

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/c3eru/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b triton -g default,-mips,-darwin,-notdefault
 git clone https://github.com/c3eru/local_manifest --depth 1 -b c3eru-patch-1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_chime-userdebug
+lunch awaken_chime-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=mobx
 export KBUILD_BUILD_HOST=builder-ci-task-kang-moment
@@ -14,4 +14,4 @@ export BUILD_HOSTNAME=builder-ci-task-kang-moment
 make bacon -j8
 # end 
 
-# build 4
+# build 1
