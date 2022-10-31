07f900ca7389d7e6dddec34130bf5281357833e9
c3eru <96016081+c3eru@users.noreply.github.com>
2022-10-31 12:50:44 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,11 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/c3eru/local_manifest --depth 1 -b anuu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-git clone --depth=1 https://github.com/c3eru/dtchime -b lineage-19.1 device/xiaomi/chime
-git clone --depth=1 https://github.com/frstprjkt/vendor_xiaomi_chime -b twelve vendor/xiaomi/chime
-git clone --depth=1 https://github.com/greenforce-project/kernel_xiaomi_citrus_sm6115 -b rvc kernel/xiaomi/chime
-git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton
 
 # build rom
 source build/envsetup.sh
-lunch lineage_chime-userdebug
+lunch derp_chime-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=mobx
 export KBUILD_BUILD_HOST=builder-ci-task-kang-moment
@@ -18,4 +14,4 @@ export BUILD_HOSTNAME=builder-ci-task-kang-moment
 make bacon -j8
 # end 
 
-# build 3
+# build 
