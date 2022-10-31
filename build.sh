b2853b4958c4b9a968838c5e2af3e465cbf8bd7d
c3eru <96016081+c3eru@users.noreply.github.com>
2022-10-31 20:16:18 +0700

    Test

--- build.sh
+++ build.sh
@@ -11,7 +11,8 @@ export KBUILD_BUILD_USER=mobx
 export KBUILD_BUILD_HOST=builder-ci-task-kang-moment
 export BUILD_USERNAME=mobx
 export BUILD_HOSTNAME=builder-ci-task-kang-moment
+export WITH_GMS=true
 make bacon -j8
 # end 
 
-# build 1
+# build 2
