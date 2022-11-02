951af3c8d327de3f154b8588aa767b45afd8df5c
c3eru <96016081+c3eru@users.noreply.github.com>
2022-11-02 17:22:33 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ export KBUILD_BUILD_HOST=builder-ci-task-kang-moment
 export BUILD_USERNAME=mobx
 export BUILD_HOSTNAME=builder-ci-task-kang-moment
 export WITH_GMS=true
-make Wallpapers2
+make ThemePicker
 # make bacon -j8
 # end 
 
