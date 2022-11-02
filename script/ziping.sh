317474ad3e4abe64e5a1697e784a3a4694dbd850
c3eru <96016081+c3eru@users.noreply.github.com>
2022-11-02 16:59:51 +0700

    [skip ci]

--- script/ziping.sh
+++ script/ziping.sh
@@ -34,9 +34,8 @@ p404=$(ls out/target/product/$device/?.*zip || true)
 cipher=$(ls out/target/product/$device/CipherOS-*-OTA-*.zip || true)
 rm -rf $engzip $otazip $awaken $octavi $p404 $cipher
 file_name=$(basename out/target/product/$device/*.zip)
-DL_LINK=https://nfsproject.projek.workers.dev/0:/$name_rom/$device/$file_name
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip mobx:$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy out/target/product/chime/system_ext/priv-app/Settings/Settings.apk mobx:$CIRRUS_WORKING_DIR -P
+DL_LINK=https://file.cloudmobx.workers.dev/Apps/Derp-13/ThemePicker.apk
+rclone copy out/target/product/chime/system_ext/priv-app/ThemePicker/*.apk mobx:Apps/Derp-13 -P
 cd $WORKDIR/rom/$name_rom/out/target/product/$device
 echo -e \
 "
