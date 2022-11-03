0ae070d8bbdfaeced96349eb2872462a6f30f69a
c3eru <96016081+c3eru@users.noreply.github.com>
2022-11-03 19:39:33 +0700

    Awaken

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/c3eru/local_manifest --depth 1 -b anuu .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b triton -g default,-mips,-darwin,-notdefault
+git clone https://github.com/c3eru/local_manifest --depth 1 -b awaken .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_chime-userdebug
+lunch awaken_chime-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=mobx
 export KBUILD_BUILD_HOST=builder-ci-task-kang-moment
